4d9c746657fb80f8c6c4ba8bc3c0f82a8fa775f5
zedparman <ze.dparman@gmail.com>
2025-05-21 23:56:26 -0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <div className="container mt-5">
       <h1>Welcome to the Next.js Template</h1>
-      <p>This is a simple placeholder page.</p>
+      <p>This is a simple  page.</p>
     </div>
   );
 }
