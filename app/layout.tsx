fd9250a507a8c5b9fad6813f9a17590df0eac915
zedparman <ze.dparman@gmail.com>
2025-05-22 01:41:14 -0700

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Navbar from "../components/Navbar";
+import ClientRoot from "../components/ClientRoot";
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -16,8 +17,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
-        <Navbar />
-        {children}
+        <ClientRoot>
+          <Navbar />
+          {children}
+        </ClientRoot>
       </body>
     </html>
   );
